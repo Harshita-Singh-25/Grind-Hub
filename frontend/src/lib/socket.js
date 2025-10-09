a6910fcd6df56b1ef1da5723fa283c9be438e7c1
Harshita Singh <harshitaks2005@gmail.com>
2025-10-09 21:12:06 +0530

    the axios and socket lib read the backend url from the environment varable instead of hardcoded local address

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 import { useAuthStore } from '../store/useAuthStore';
 
-const URL = 'http://localhost:5001';
+const URL = import.meta.env.VITE_SOCKET_URL; // Use the SOCKET URL from environment variables
 let socket;
 
 // Initialize the socket connection with the user ID
