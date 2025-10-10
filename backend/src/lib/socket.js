81a7cda77ab9a5c420948010e1d4e6a004e50bb9
Harshita Singh <harshitaks2005@gmail.com>
2025-10-10 09:16:46 +0530

    fixed socket cors to allow the deployed frontend

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,9 +5,18 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://grind-hub-frontend.vercel.app",
+  "https://grind-hub-frontend.onrender.com"
+];
+
+
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin:  allowedOrigins,
     credentials: true, // Add this line
     methods: ["GET", "POST"]
   },
