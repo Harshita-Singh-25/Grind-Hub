ce3e2f3ec8a968c831ee3738e29ed55a396b00c3
Harshita Singh <harshitaks2005@gmail.com>
2025-10-10 10:26:13 +0530

    harcoding the backend url , so that frontend calls it correctly and adding the isDev var

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+const isDevelopment = import.meta.env.MODE === "development";
+
+
 export const axiosInstance = axios.create({
   //baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   //baseURL: import.meta.env.VITE_BACKEND_URL,
